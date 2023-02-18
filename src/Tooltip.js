94ec9774b87ae146d95f57a7e90338d16681236f
Saurabh Panja <saurabh.panja@phamax.ch>
2023-02-19 02:21:00 +0530

    tooltip styling done

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -3,7 +3,13 @@ import "./Tooltip.css";
 function Tooltip() {
   return (
     <div className="tooltip-container">
-      Hello
+      <div className="tooltip tooltip-left">Tooltip Left</div>
+      <div className="tooltip-central-col">
+        <div className="tooltip tooltip-down">Tooltip Down</div>
+        <button className="tooltip-btn">Hover over me!</button>
+        <div className="tooltip tooltip-up">Tooltip &nbsp;&nbsp;Up</div>
+      </div>
+      <div className="tooltip tooltip-right">Tooltip Right</div>
     </div>
   );
 }
